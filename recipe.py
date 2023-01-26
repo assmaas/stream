60e6c46e87237f6bbc0bfe28b50b6c039d59b646
assmaas <72825299+assmaas@users.noreply.github.com>
2023-01-26 16:22:48 +0100

    Update and rename ok.py to recipe.py

---
+++ recipe.py
@@ -0,0 +1,10 @@
+#import libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+# adding title in streamlit
+st.sidebar.markdown(f"<span style='color: black;font-size: 36px;font-weight: bold;'> Recettes </span>", unsafe_allow_html=True)
+
+st.sidebar.info("Hello Amal S.")
